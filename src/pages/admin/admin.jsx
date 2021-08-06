7ab559c75ce6238562446432c965560948adf454
hwt5813780 <291245768@qq.com>
2021-08-06 22:47:25 +0800

    1.0

---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import {connect} from "react-redux";
+
+class Admin extends Component {
+    render() {
+        return (
+            <div>
+                admin
+            </div>
+        )
+    }
+}
+
+export default connect(null,null)(Admin);
